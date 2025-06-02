23bdcb6379843422cda9a5cc04571fa45b3fa65b
Julien Bloino <julien.bloino@sns.it>
2025-06-02 11:19:00 +0200

    New type to store spectrosc. conversion functions
    `physics` now provide a new derived type and associated instance to
    store and use common conversion methods related more specifically to
    computational spectroscopy.
    The module has been reorganized to be in alphabetical order to
    simplify future maintenances.
    The documentation has also been improved.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,10 @@
 # Changelog
 
-## 0.25.05
+## 0.25.06
 
 ### Added
+* ***physics***: New derived type `SpectroConv` and new instance `spec_conv` to store common conversion operations related to computational spectroscopy.
+* ***physics***: New type-bound procedure `mwq2q` to facilitate conversions of quantity expressed with respect to normal coordinates.
 * ***vibrational***: New function interface `full_boltz_pop` to compute the analytic total Boltzmann population for a given set of frequencies at any temperature.
 
 ### Fixed
--- src/core/physics.f90
+++ src/core/physics.f90
@@ -6,9 +6,12 @@ module physics
     !!
     !! - PhysFact : derived-type with physical conversion factors
     !! - phys_conv: Instance of PhysFact
+    !! - SpectroConv : derived-type containing conversion methods common
+    !!   to spectroscopic applications
+    !! - spec_conv: Instance of `SpectroConv`.
     !!
-    !! Most functions in phys_conv can operate in reverse to convert
-    !! backward.
+    !! Most functions in `phys_conv` and `spec_conv` can operate in
+    !! reverse to convert backward.
     use iso_fortran_env, only: real32, real64
 
     implicit none
@@ -50,11 +53,13 @@ module physics
     real(real64), parameter, private :: &
         pi = 4.0_real64*atan(1.0_real64), &
         hbar_amu_Ang_s1 = planck*1.0e20_real64/(2.0_real64*pi*u_at_mass), &
-        !! Reduced Planck constant in amu.Ang^2.s^-1
+            !! Reduced Planck constant in amu.Ang^2.s^-1
+        hbar_amu_a0_s1 = hbar_amu_Ang_s1 / bohr_radius**2, &
+            !! Reduced Planck constant in amu.a0^2.s^-1
         hc = planck*slight, &
-        !! h*c, in J.cm
+            !! h*c, in J.cm
         factG = 2.0_real64*pi*slight/hbar_amu_Ang_s1
-        !! 2 pi c / hbar in cm.amu^-1.Ang^-2
+            !! 2 pi c / hbar in cm.amu^-1.Ang^-2
 
     type, public :: PhysFact
         contains
@@ -80,85 +85,22 @@ module physics
                 d_conv_au_to_u_mass
     end type PhysFact
 
+    type, public :: SpectroConv
+        contains
+            procedure, nopass, private :: &
+                s_conv_mwq2q, d_conv_mwq2q
+            generic, public :: mwq2q => s_conv_mwq2q, d_conv_mwq2q
+    end type SpectroConv
+
     type(PhysFact), public :: phys_conv
+    type(SpectroConv), public :: spec_conv
 
 contains
 
 ! ======================================================================
 
-elemental real(real32) function s_conv_bohr_to_Ang(x, reverse)
-    real(real32), intent(in) :: x
-    logical, intent(in), optional :: reverse
-    logical :: do_reverse
-    if (present(reverse)) then
-        do_reverse = reverse
-    else
-        do_reverse = .false.
-    end if
-    if (do_reverse) then
-        s_conv_bohr_to_Ang = x / real(bohr_radius, kind=real32)
-    else
-        s_conv_bohr_to_Ang = x * real(bohr_radius, kind=real32)
-    end if
-end function s_conv_bohr_to_Ang
-
-! ======================================================================
-
-elemental real(real64) function d_conv_bohr_to_Ang(x, reverse)
-    real(real64), intent(in) :: x
-    logical, intent(in), optional :: reverse
-    logical :: do_reverse
-    if (present(reverse)) then
-        do_reverse = reverse
-    else
-        do_reverse = .false.
-    end if
-    if (do_reverse) then
-        d_conv_bohr_to_Ang = x / bohr_radius
-    else
-        d_conv_bohr_to_Ang = x * bohr_radius
-    end if
-end function d_conv_bohr_to_Ang
-
-! ======================================================================
-
-elemental real(real32) function s_conv_au_to_u_mass(x, reverse)
-    real(real32), intent(in) :: x
-    logical, intent(in), optional :: reverse
-    logical :: do_reverse
-    if (present(reverse)) then
-        do_reverse = reverse
-    else
-        do_reverse = .false.
-    end if
-    if (do_reverse) then
-        s_conv_au_to_u_mass = x / real(e_mass/u_at_mass, kind=real32)
-    else
-        s_conv_au_to_u_mass = x * real(e_mass/u_at_mass, kind=real32)
-    end if
-end function s_conv_au_to_u_mass
-
-! ======================================================================
-
-elemental real(real64) function d_conv_au_to_u_mass(x, reverse)
-    real(real64), intent(in) :: x
-    logical, intent(in), optional :: reverse
-    logical :: do_reverse
-    if (present(reverse)) then
-        do_reverse = reverse
-    else
-        do_reverse = .false.
-    end if
-    if (do_reverse) then
-        d_conv_au_to_u_mass = x / (e_mass/u_at_mass)
-    else
-        d_conv_au_to_u_mass = x * e_mass/u_at_mass
-    end if
-end function d_conv_au_to_u_mass
-
-! ======================================================================
-
 elemental real(real32) function s_conv_amu_to_kg(x, reverse)
+    !! Convert mass from unified (u) to kilogram (32-bit).
     real(real32), intent(in) :: x
     logical, intent(in), optional :: reverse
     logical :: do_reverse
@@ -177,6 +119,7 @@ end function s_conv_amu_to_kg
 ! ======================================================================
 
 elemental real(real64) function d_conv_amu_to_kg(x, reverse)
+    !! Convert mass from unified (u) to kilogram (64-bit).
     real(real64), intent(in) :: x
     logical, intent(in), optional :: reverse
     logical :: do_reverse
@@ -194,80 +137,8 @@ end function d_conv_amu_to_kg
 
 ! ======================================================================
 
-elemental real(real32) function s_conv_e_to_C(x, reverse)
-    real(real32), intent(in) :: x
-    logical, intent(in), optional :: reverse
-    logical :: do_reverse
-    if (present(reverse)) then
-        do_reverse = reverse
-    else
-        do_reverse = .false.
-    end if
-    if (do_reverse) then
-        s_conv_e_to_C = x / real(e_charge, kind=real32)
-    else
-        s_conv_e_to_C = x * real(e_charge, kind=real32)
-    end if
-end function s_conv_e_to_C
-
-! ======================================================================
-
-elemental real(real64) function d_conv_e_to_C(x, reverse)
-    real(real64), intent(in) :: x
-    logical, intent(in), optional :: reverse
-    logical :: do_reverse
-    if (present(reverse)) then
-        do_reverse = reverse
-    else
-        do_reverse = .false.
-    end if
-    if (do_reverse) then
-        d_conv_e_to_C = x / e_charge
-    else
-        d_conv_e_to_C = x * e_charge
-    end if
-end function d_conv_e_to_C
-
-! ======================================================================
-
-elemental real(real32) function s_conv_cal_to_J(x, reverse)
-    real(real32), intent(in) :: x
-    logical, intent(in), optional :: reverse
-    logical :: do_reverse
-    if (present(reverse)) then
-        do_reverse = reverse
-    else
-        do_reverse = .false.
-    end if
-    if (do_reverse) then
-        s_conv_cal_to_J = x / real(calorie, kind=real32)
-    else
-        s_conv_cal_to_J = x * real(calorie, kind=real32)
-    end if
-end function s_conv_cal_to_J
-
-! ======================================================================
-
-elemental real(real64) function d_conv_cal_to_J(x, reverse)
-    real(real64), intent(in) :: x
-    logical, intent(in), optional :: reverse
-    logical :: do_reverse
-    if (present(reverse)) then
-        do_reverse = reverse
-    else
-        do_reverse = .false.
-    end if
-    if (do_reverse) then
-        d_conv_cal_to_J = x / calorie
-    else
-        d_conv_cal_to_J = x * calorie
-    end if
-end function d_conv_cal_to_J
-
-! ======================================================================
-
 elemental real(real32) function s_conv_au_freq_to_cm1(x, reverse)
-    !! Convert frequency in atomic unit to energy in cm-1.
+    !! Convert frequency from atomic unit to wavenumber (32-bit).
     real(real32), intent(in) :: x
     logical, intent(in), optional :: reverse
     logical :: do_reverse
@@ -286,7 +157,7 @@ end function s_conv_au_freq_to_cm1
 ! ======================================================================
 
 elemental real(real64) function d_conv_au_freq_to_cm1(x, reverse)
-    !! Convert frequency in atomic unit to energy in cm-1.
+    !! Convert frequency from atomic unit to wavenumber (32-bit).
     real(real64), intent(in) :: x
     logical, intent(in), optional :: reverse
     logical :: do_reverse
@@ -304,7 +175,211 @@ end function d_conv_au_freq_to_cm1
 
 ! ======================================================================
 
+elemental real(real32) function s_conv_au_to_u_mass(x, reverse)
+    !! Convert mass from atomic unit to unified (32-bit).
+    real(real32), intent(in) :: x
+    logical, intent(in), optional :: reverse
+    logical :: do_reverse
+    if (present(reverse)) then
+        do_reverse = reverse
+    else
+        do_reverse = .false.
+    end if
+    if (do_reverse) then
+        s_conv_au_to_u_mass = x / real(e_mass/u_at_mass, kind=real32)
+    else
+        s_conv_au_to_u_mass = x * real(e_mass/u_at_mass, kind=real32)
+    end if
+end function s_conv_au_to_u_mass
+
+! ======================================================================
+
+elemental real(real64) function d_conv_au_to_u_mass(x, reverse)
+    !! Convert mass from atomic unit to unified (64-bit).
+    real(real64), intent(in) :: x
+    logical, intent(in), optional :: reverse
+    logical :: do_reverse
+    if (present(reverse)) then
+        do_reverse = reverse
+    else
+        do_reverse = .false.
+    end if
+    if (do_reverse) then
+        d_conv_au_to_u_mass = x / (e_mass/u_at_mass)
+    else
+        d_conv_au_to_u_mass = x * e_mass/u_at_mass
+    end if
+end function d_conv_au_to_u_mass
+
+! ======================================================================
+
+elemental real(real32) function s_conv_bohr_to_Ang(x, reverse)
+    !! Convert length from atomic unit to Angstrom (32-bit).
+    real(real32), intent(in) :: x
+    logical, intent(in), optional :: reverse
+    logical :: do_reverse
+    if (present(reverse)) then
+        do_reverse = reverse
+    else
+        do_reverse = .false.
+    end if
+    if (do_reverse) then
+        s_conv_bohr_to_Ang = x / real(bohr_radius, kind=real32)
+    else
+        s_conv_bohr_to_Ang = x * real(bohr_radius, kind=real32)
+    end if
+end function s_conv_bohr_to_Ang
+
+! ======================================================================
+
+elemental real(real64) function d_conv_bohr_to_Ang(x, reverse)
+    !! Convert length from atomic unit to Angstrom (64-bit).
+    real(real64), intent(in) :: x
+    logical, intent(in), optional :: reverse
+    logical :: do_reverse
+    if (present(reverse)) then
+        do_reverse = reverse
+    else
+        do_reverse = .false.
+    end if
+    if (do_reverse) then
+        d_conv_bohr_to_Ang = x / bohr_radius
+    else
+        d_conv_bohr_to_Ang = x * bohr_radius
+    end if
+end function d_conv_bohr_to_Ang
+
+! ======================================================================
+
+elemental real(real32) function s_conv_cal_to_J(x, reverse)
+    !! Convert energy from calorie to Joule (32-bit).
+    real(real32), intent(in) :: x
+    logical, intent(in), optional :: reverse
+    logical :: do_reverse
+    if (present(reverse)) then
+        do_reverse = reverse
+    else
+        do_reverse = .false.
+    end if
+    if (do_reverse) then
+        s_conv_cal_to_J = x / real(calorie, kind=real32)
+    else
+        s_conv_cal_to_J = x * real(calorie, kind=real32)
+    end if
+end function s_conv_cal_to_J
+
+! ======================================================================
+
+elemental real(real64) function d_conv_cal_to_J(x, reverse)
+    !! Convert energy from calorie to Joule (64-bit).
+    real(real64), intent(in) :: x
+    logical, intent(in), optional :: reverse
+    logical :: do_reverse
+    if (present(reverse)) then
+        do_reverse = reverse
+    else
+        do_reverse = .false.
+    end if
+    if (do_reverse) then
+        d_conv_cal_to_J = x / calorie
+    else
+        d_conv_cal_to_J = x * calorie
+    end if
+end function d_conv_cal_to_J
+
+! ======================================================================
+
+elemental real(real32) function s_conv_dE_au_to_cgs(x, derord)
+    !! Convert energy derivatives from atomic unit to cgs system.
+    !!
+    !! Converts energy derivatives from pure atomic derivatives to cgs
+    !! system.
+    !! The conversion factors provides derivatives with unit
+    !! cm<sup>-(derorder/2).</sup>
+    !!
+    !! @note "version"
+    !! 32-bit version
+    !! @endnote
+    real(real32), intent(in) :: x
+    integer, intent(in), optional :: derord
+        !! Derivative order
+    integer :: dn
+    if (present(derord)) then
+        dn = max(0, derord)
+    else
+        dn = 2
+    end if
+    s_conv_dE_au_to_cgs = x &
+        * real((E_hartree/hc)/(sqrt(factG)*bohr_radius)**dn, kind=real32)
+end function s_conv_dE_au_to_cgs
+
+! ======================================================================
+
+elemental real(real64) function d_conv_dE_au_to_cgs(x, derord)
+    !! Convert energy derivatives from atomic unit to cgs system.
+    !!
+    !! Converts energy derivatives from pure atomic derivatives to cgs
+    !! system.
+    !! The conversion factors provides derivatives with unit
+    !! cm<sup>-(derorder/2).</sup>
+    !!
+    !! @note "version"
+    !! 64-bit version
+    !! @endnote
+    real(real64), intent(in) :: x
+    integer, intent(in), optional :: derord
+    !! Derivative order
+    integer :: dn
+    if (present(derord)) then
+        dn = max(0, derord)
+    else
+        dn = 2
+    end if
+    d_conv_dE_au_to_cgs = x * E_hartree/hc/(sqrt(factG)*bohr_radius)**dn
+end function d_conv_dE_au_to_cgs
+
+! ======================================================================
+
+elemental real(real32) function s_conv_e_to_C(x, reverse)
+    !! Convert charge from atomic unit to Coulomb (32-bit).
+    real(real32), intent(in) :: x
+    logical, intent(in), optional :: reverse
+    logical :: do_reverse
+    if (present(reverse)) then
+        do_reverse = reverse
+    else
+        do_reverse = .false.
+    end if
+    if (do_reverse) then
+        s_conv_e_to_C = x / real(e_charge, kind=real32)
+    else
+        s_conv_e_to_C = x * real(e_charge, kind=real32)
+    end if
+end function s_conv_e_to_C
+
+! ======================================================================
+
+elemental real(real64) function d_conv_e_to_C(x, reverse)
+    !! Convert charge from atomic unit to Coulomb (64-bit).
+    real(real64), intent(in) :: x
+    logical, intent(in), optional :: reverse
+    logical :: do_reverse
+    if (present(reverse)) then
+        do_reverse = reverse
+    else
+        do_reverse = .false.
+    end if
+    if (do_reverse) then
+        d_conv_e_to_C = x / e_charge
+    else
+        d_conv_e_to_C = x * e_charge
+    end if
+end function d_conv_e_to_C
+
+! ======================================================================
+
 elemental real(real32) function s_conv_hartree_to_J(x, reverse)
+    !! Convert energy from atomic unit to Joule (32-bit).
     real(real32), intent(in) :: x
     logical, intent(in), optional :: reverse
     logical :: do_reverse
@@ -323,6 +398,7 @@ end function s_conv_hartree_to_J
 ! ======================================================================
 
 elemental real(real64) function d_conv_hartree_to_J(x, reverse)
+    !! Convert energy from atomic unit to Joule (64-bit).
     real(real64), intent(in) :: x
     logical, intent(in), optional :: reverse
     logical :: do_reverse
@@ -340,36 +416,103 @@ end function d_conv_hartree_to_J
 
 ! ======================================================================
 
-elemental real(real32) function s_conv_dE_au_to_cgs(x, derord)
-    !! Return energy derivatives from au to cm^-(derorder/2).
+elemental real(real32) function s_conv_mwq2q(x, omega, reverse)
+    !! Convert mass-weighted normal coordinates Q to dimensionless q.
+    !!
+    !! Converts between mass-weighted and dimensionless normal
+    !! coordinates, considering the following relationship,
+    !! \[ Q_i = \sqrt{\hbar}{2 \pi c \omega_i} q_i\]
+    !! with $\omega$ in wavenumbers (cm<sup>-1</sup>)
+    !!
+    !! If $\omega$ is not provided, only the constant factor is
+    !! calculated.
+    !!
+    !! @note "version"
+    !! 32-bit version.
+    !! @endnote
     real(real32), intent(in) :: x
-    integer, intent(in), optional :: derord
-    !! Derivative order
-    integer :: dn
-    if (present(derord)) then
-        dn = max(0, derord)
+    real(real32), intent(in), optional :: omega
+        !! Harmonic frequency, in cm<sup>-1</sup>
+    logical, intent(in), optional :: reverse
+
+    ! factor = sqrt(hbar / (2pi*c))
+    ! hbar needs to be convert to u.a_0^2.s-1 for conversion of properties
+    ! in standard atomic units
+    real(real64), parameter :: &
+        factor = sqrt(hbar_amu_a0_s1/(2.0_real64*pi*slight))
+    real(real64), parameter :: m2ang = 1.0e10
+    logical :: do_reverse
+
+    if (present(reverse)) then
+        do_reverse = reverse
     else
-        dn = 2
+        do_reverse = .false.
     end if
-    s_conv_dE_au_to_cgs = x &
-        * real((E_hartree/hc)/(sqrt(factG)*bohr_radius)**dn, kind=real32)
-end function s_conv_dE_au_to_cgs
+
+    if (do_reverse) then
+        if (present(omega)) then
+            s_conv_mwq2q = x * real(factor, kind=real32) / sqrt(omega)
+        else
+            s_conv_mwq2q = x * real(factor, kind=real32)
+        end if
+    else
+        if (present(omega)) then
+            s_conv_mwq2q = x * sqrt(omega) / real(factor, kind=real32)
+        else
+            s_conv_mwq2q = x / real(factor, kind=real32)
+        end if
+    end if
+end function s_conv_mwq2q
 
 ! ======================================================================
 
-elemental real(real64) function d_conv_dE_au_to_cgs(x, derord)
-    !! Return energy derivatives from au to cm^-(derorder/2).
+elemental real(real64) function d_conv_mwq2q(x, omega, reverse)
+    !! Convert mass-weighted normal coordinates Q to dimensionless q.
+    !!
+    !! Converts between mass-weighted and dimensionless normal
+    !! coordinates, considering the following relationship,
+    !! \[ Q_i = \sqrt{\hbar}{2 \pi c \omega_i} q_i\]
+    !! with $\omega$ in wavenumbers (cm<sup>-1</sup>)
+    !!
+    !! If $\omega$ is not provided, only the constant factor is
+    !! calculated.
+    !!
+    !! @note "version"
+    !! 64-bit version.
+    !! @endnote
     real(real64), intent(in) :: x
-    integer, intent(in), optional :: derord
-    !! Derivative order
-    integer :: dn
-    if (present(derord)) then
-        dn = max(0, derord)
+    real(real64), intent(in), optional :: omega
+        !! Harmonic frequency, in cm<sup>-1</sup>
+    logical, intent(in), optional :: reverse
+
+    ! factor = sqrt(hbar / (2pi*c))
+    ! hbar needs to be convert to u.a_0^2.s-1 for conversion of properties
+    ! in standard atomic units
+    real(real64), parameter :: &
+        factor = sqrt(hbar_amu_a0_s1/(2.0_real64*pi*slight))
+    real(real64), parameter :: m2ang = 1.0e10
+    logical :: do_reverse
+
+    if (present(reverse)) then
+        do_reverse = reverse
     else
-        dn = 2
+        do_reverse = .false.
     end if
-    d_conv_dE_au_to_cgs = x * E_hartree/hc/(sqrt(factG)*bohr_radius)**dn
-end function d_conv_dE_au_to_cgs
+
+    if (do_reverse) then
+        if (present(omega)) then
+            d_conv_mwq2q = x * factor / sqrt(omega)
+        else
+            d_conv_mwq2q = x * factor
+        end if
+    else
+        if (present(omega)) then
+            d_conv_mwq2q = x * sqrt(omega) / factor
+        else
+            d_conv_mwq2q = x / factor
+        end if
+    end if
+end function d_conv_mwq2q
 
 ! ======================================================================
 
